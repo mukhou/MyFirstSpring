639b3235d2c13ab3ee4077a63ea9ef671687b4a2
jhini <banerjeeurmi@gmail.com>
2020-02-13 18:38:08 -0500

    Adding Cucumber

---
+++ src/test/java/cucumber/examples/java/AddStepdefs.java
@@ -0,0 +1,32 @@
+package cucumber.examples.java;/*
+User: Urmi
+Date: 2/13/2020 
+Time: 6:14 PM
+*/
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+
+public class AddStepdefs {
+    int a, b;
+    int result;
+
+    @Given("^I have two numbers (\\d+) and (\\d+)$")
+    public void iHaveTwoNumbersAnd(int arg0, int arg1) throws Throwable {
+        a = arg0;
+        b = arg1;
+    }
+
+
+    @When("^I add numbers (\\d+) and (\\d+)$")
+    public void iAddNumbersAnd(int arg0, int arg1) throws Throwable {
+       result = a + b;
+    }
+
+    @Then("^I get the result as (\\d+)$")
+    public void iGetTheResultAs(int arg0) throws Throwable {
+        Assert.assertEquals(9, arg0);
+    }
+}
---
+++ src/test/resources/cucumber/examples/java/add.feature
@@ -0,0 +1,6 @@
+Feature: Test feature
+
+  Scenario: Verify numbers addition
+    Given I have two numbers 4 and 5
+    When I add numbers 4 and 5
+    Then I get the result as 9
